99ec7692c539f36a9afa7274f7c11f21e54e9c53
elikoschwitz <e.koschwitz@googmail.com>
2022-12-20 14:10:01 +0100

    Init: in the beginning god created the heavens and the earth

---
+++ src/main/java/com/example/moviedbtask/MovieDbTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviedbtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDbTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDbTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moviedbtask/MovieDbTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviedbtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDbTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
